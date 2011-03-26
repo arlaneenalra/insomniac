d17c5ed77f548a8f0a51b1dd90b52544988d0d93
Chris Salch <emeraldd.chris@gmail.com>
2011-03-25 22:55:27 -0500

    Let's start this mess from scratch and get rid of all the junk.

--- src/main.c
+++ src/main.c
@@ -1,50 +1,47 @@
 #include <stdio.h>
 
-#include <util.h>
-#include <parser.h>
-#include <gc.h>
 
+/* /\* A simple REPL *\/ */
+/* void repl(parser_core_type *parser) { */
 
-/* A simple REPL */
-void repl(parser_core_type *parser) {
+/*     object_type *obj=0; */
+/*     bool running=1; */
 
-    object_type *obj=0;
-    bool running=1;
-
-    printf("Insomniac Scheme\n");
-    printf("sizeof(object_type) %" PRIi64 "\n", (uint64_t)sizeof(object_type));
+/*     printf("Insomniac Scheme\n"); */
+/*     printf("sizeof(object_type) %" PRIi64 "\n", (uint64_t)sizeof(object_type)); */
     
-    while(running) {
+/*     while(running) { */
 
-	printf(">");
+/* 	printf(">"); */
 	
-	obj=parse(parser, stdin);
+/* 	obj=parse(parser, stdin); */
 	
-	if(obj==parser->empty_list) {
-	    running=0;
-	}
-	output(parser, obj);
+/* 	if(obj==parser->empty_list) { */
+/* 	    running=0; */
+/* 	} */
+/* 	output(parser, obj); */
 
-	printf("\n");
-    }
+/* 	printf("\n"); */
+/*     } */
 
-}
+/* } */
 
 int main(int argc, char**argv) {
-    gc_core_type *gc=0;
-    object_type *obj=0;
+    printf("Insomniac\n");
+    /* gc_core_type *gc=0; */
+    /* object_type *obj=0; */
 
-    /* setup our garbage collector and parser */
-    gc=gc_create();
-    parser_core_type *parser=parser_create(gc);
+    /* /\* setup our garbage collector and parser *\/ */
+    /* gc=gc_create(); */
+    /* parser_core_type *parser=parser_create(gc); */
 
-    repl(parser);
+    /* repl(parser); */
 
-    obj=0;
-    /* clean up the garbage collector and parser */
-    parser_destroy(parser);
-    gc_stats(gc);
-    gc_destroy(gc);
+    /* obj=0; */
+    /* /\* clean up the garbage collector and parser *\/ */
+    /* parser_destroy(parser); */
+    /* gc_stats(gc); */
+    /* gc_destroy(gc); */
 
     return 0;
 }

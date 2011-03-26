c29e8abbfd57551bf82e27d286eece5acb00a810
Chris Salch <emeraldd.chris@gmail.com>
2011-03-26 03:22:31 -0500

    minor adjustment to count.sh

--- count.sh
+++ count.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-echo "Cleaning . . ."
-make clean
-echo
+#echo "Cleaning . . ."
+#make clean
+#echo
 
 echo "Source Statistics:"
-wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v 'test\/test_eyeball\|CMake\|build/'`
+wc `find ./src -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v 'test\/test_eyeball\|CMake\|build/'`
 
 echo
 echo "Commit Count: " `git log | grep '^commit' | wc -l`

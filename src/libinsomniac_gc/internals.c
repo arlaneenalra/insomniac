3bdc30c2bf77e437c54af1d89fa316359a5e2c47
Chris Salch <emeraldd.chris@gmail.com>
2011-06-04 23:13:33 -0500

    Moving offset code into a separate file.

--- src/libinsomniac_gc/internals.c
+++ src/libinsomniac_gc/internals.c
@@ -95,22 +95,3 @@ void pre_alloc(gc_ms_type *gc) {
     gc_unprotect(gc);
 }
 
-
-/* return a pointer to the meta object for a given object */
-meta_obj_type *meta_from_obj(void *obj) {
-    /* make sure a null stays a null */
-    if(!obj) {
-        return 0;
-    }
-
-    return (meta_obj_type *)(((uint8_t *)obj)-offsetof(meta_obj_type, obj));
-}
-
-/* return a pointer to the given object for a given meta object */
-void *obj_from_meta(meta_obj_type *meta) {
-    if(!meta) {
-        return 0;
-    }
-
-    return &(meta->obj);
-}

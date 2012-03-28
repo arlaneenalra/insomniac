f1288a458d2ce5413e68024ba19d8e035a288653
arlaneenalra <emeraldd.chris@gmail.com>
2012-03-27 23:37:22 -0500

    Splitting out list and vector code.

--- src/libinsomniac_vm/libinsomniac_vm_instructions/vm_instructions_internal.h
+++ src/libinsomniac_vm/libinsomniac_vm_instructions/vm_instructions_internal.h
@@ -12,7 +12,6 @@ void op_lit_char(vm_internal_type *vm);
 void op_lit_empty(vm_internal_type *vm);
 void op_lit_true(vm_internal_type *vm);
 void op_lit_false(vm_internal_type *vm);
-
 void op_lit_string(vm_internal_type *vm);
 void op_lit_symbol(vm_internal_type *vm);
 void op_make_symbol(vm_internal_type *vm);
@@ -20,5 +19,17 @@ void op_make_symbol(vm_internal_type *vm);
 /* logical opreations and comparisons */
 void op_eq(vm_internal_type *vm);
 void op_not(vm_internal_type *vm);
+
+/* vector operations */
+void op_make_vector(vm_internal_type *vm);
+void op_vector_set(vm_internal_type *vm);
+void op_vector_ref(vm_internal_type *vm);
+
+/* list operations */
+void op_cons(vm_internal_type *vm);
+void op_car(vm_internal_type *vm);
+void op_cdr(vm_internal_type *vm);
+void op_set_car(vm_internal_type *vm);
+void op_set_cdr(vm_internal_type *vm);
 
 #endif

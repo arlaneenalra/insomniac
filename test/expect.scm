ccad18979a9d57baa61604e788b5510283c9d24b
arlaneenalra <emeraldd.chris@gmail.com>
2014-12-12 08:15:56 -0600

    Now that we have equals use it.

--- test/expect.scm
+++ test/expect.scm
@@ -2,20 +2,39 @@
 ;;; Some simple testing procedures
 ;;;
 
+(define (fail)
+  (display ":FAILED")
+  (newline)
+  (emergency-exit 1))
+
+(define (pass)
+  (display ":PASSED")
+  (newline))
+
 ;; A very crude test case handler
 (define (expect-true name test)
   (display name)
 
-  (if (test)
-    (display ":PASSED")
-    (begin
-      (display ":FAILED")
-      (newline)
-      (emergency-exit 1)))
-  (newline))
+  (if (test) (pass) (fail)))
 
 (define (expect-false name test)
   (expect-true name
                (lambda () (not (test)))))
+
+(define (expect name test expected)
+  (display name)
+  (define result (test))
+
+  (if (equal? result expected)
+    (pass)
+    (begin
+      (newline)
+      (display " Expected: ")
+      (display expected)
+      (newline)
+      (display " Result: ")
+      (display result)
+      (newline)
+      (fail))))
 
 

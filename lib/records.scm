58fa33df640549ae3c3c814edb5003c014834b4f
arlaneenalra <emeraldd.chris@gmail.com>
2014-12-19 08:28:20 -0600

    Merging in new list functions.

--- lib/records.scm
+++ lib/records.scm
@@ -4,38 +4,49 @@
 
 ;; Note: these procedures are actually used by the compiler
 
+;; Id value for next record type 
+(define *record-id* 0)
+
+;; returns a record id unique in the system
+(define (next-record-type)
+  (define type-id *record-id*)
+  (set! *record-id* (+ 1 *record-id*))
+  type-id)
+
 ;; Returns true if obj is a record
 (define (record? obj) (asm(obj) record?))
 
-;; creates a record primitive with the given size
+
+;; creates an a record primitive with the given size
 (define (make-record size)
   (asm (size) record))
 
-;; read index in a record
-(define (record-type-id rec)
-  (asm 0 (rec) idx@))
+;; accessors for record fields
+(define (record-ref rec idx)
+  (asm (idx) (rec) idx@))
 
-;; set value at index in record
-(define (set-record-type-id rec type)
-  (asm 0 (type) (rec) idx! ()))
+(define (record-set! rec idx obj)
+  (asm (idx) (type) (rec) idx! ()))
 
-;; Generic record constructor
+  ;; Generic record constructor
 (define (record-constructor type size)
   (define rec (make-record size))
   (set-record-type-id rec type)
   rec)
 
 ;; The internals of define-record-type
-(define (record-type-factory rec-def)
- 
-  ;; extract clauses
+(define (record-type-factory . args)
+  (define rec-def (car args))
+
+  (define type-id (next-record-type))
+
   (define name-clause (car rec-def))
   (define const-clause  (car (cdr rec-def)))
   (define pred-clause (car (cdr (cdr rec-def))))
   (define fields-clause (cdr (cdr (cdr rec-def))))
 
-  ;; Setup the constructor
-  (define field-count (- (length const-clause) 1))
+  ;; Setup the constructor .. this is subtlely +1 by choice
+  ;; (define field-count (length const-clause))
 
   (display "Setting up define-record")
   (newline)

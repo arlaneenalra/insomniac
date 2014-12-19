b82de5469c31b7bded57b3749de4c555c960c173
arlaneenalra <emeraldd.chris@gmail.com>
2014-12-18 23:01:50 -0600

    Implement assoc assq and assv

--- test/test_lists.scm
+++ test/test_lists.scm
@@ -31,11 +31,33 @@
         (lambda () (cons '(a) '(b c d)))
         '((a) b c d))
 
-(display (cons "a" '(b c d)))
-(newline)
-
 (expect "Add to a list 2"
         (lambda () (cons "a" '(b c d)))
         '("a" b c d))
 
+
+(next-suite "member tests")
+
+(define test-list '(a b c 1 2 3 "A" "b" "c"))
+
+(expect "member should return a sublist"
+        (lambda () (member 3 test-list))
+        '(3 "A" "b" "c"))
+
+(expect "memq should return a sublist"
+        (lambda () (memq 3 test-list))
+        '(3 "A" "b" "c"))
+
+(expect "memv should return a sublist"
+        (lambda () (memq 3 test-list))
+        '(3 "A" "b" "c"))
+
+(define alist '((a 1) (b 2) (ccc 3)))
+
+(expect "assoc should return the list entry"
+        (lambda () (assoc 'b alist))
+        '(b 2))
+
+(expect-false "assoc should return false if not found"
+          (lambda () (assoc 'd alist)))
 

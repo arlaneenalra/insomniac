b3c9a6dd378bd409b19c7c60c04d2c8fcce86483
arlaneenalra <emeraldd.chris@gmail.com>
2014-12-23 21:24:37 -0600

    And define-record-type works!

--- lib/records.scm
+++ lib/records.scm
@@ -2,7 +2,7 @@
 ;;; Basic procedures used in the definition of records.
 ;;; 
 
-;; Note: these procedures are actually used by the compiler
+;; This code is horrible, I'll need to revisit it later 
 
 ;; Id value for next record type 
 (define *record-id* 0)
@@ -26,18 +26,70 @@
   (asm (idx) (rec) idx@))
 
 (define (record-set! rec idx obj)
-  (asm (idx) (type) (rec) idx! ()))
+  (asm (idx) (obj) (rec) idx! ()))
 
-  ;; Generic record constructor
-(define (record-constructor type size)
-  (define rec (make-record size))
-  (set-record-type-id rec type)
-  rec)
+;; Record constructor factory
+(define (record-constructor-factory type-id size)
+  ;; account for the type id
+  (set! size (+ 1 size))
 
+  ;; set all values in a record
+  (define (set-values rec idx field-list)
+    (if (null? field-list)
+      rec
+      (begin
+        (record-set! rec idx (car field-list))
+        (set-values rec (+ 1 idx) (cdr field-list)))))
+
+  
+  ;; the actual constructor method
+  (lambda fields
+    (define rec (make-record size))
+    (record-set! rec 0 type-id)
+    (set-values rec 1 fields)))
+
+;; define a record predicate factory
+(define (record-predicate-factory type-id)
+  (lambda (obj)
+    (and (record? obj) (eq? type-id (record-ref obj 0)))))
+
+;; return a record field getter
+(define (record-getter-factory idx)
+  (lambda (obj) (record-ref obj idx)))
+
+;; return a record field setter
+(define (record-setter-factory idx)
+  (lambda (obj value) (record-set! obj idx value)))
+
+;; setup getter/setters 
+(define (record-field-factory bindings idx accessor)
+  (cons
+    (cons (car accessor) (record-getter-factory idx))
+    (if (null? (cdr accessor))
+      bindings
+      (cons
+        (cons (car (cdr accessor)) (record-setter-factory idx))
+        bindings))))
+
+;; Walk list of fields and build accessors as needed
+(define (record-accessor-factory bindings idx fields access)
+  (if (null? fields) 
+    bindings
+    (begin
+      ;; retrieve access for this field
+      (define accessors
+        (assoc (car fields) access))
+
+      (record-accessor-factory
+        (if (not accessors)
+          bindings
+          (record-field-factory bindings idx (cdr accessors)))
+        (+ 1 idx) (cdr fields) access))))
+
+;; Note: This binding/symbol is used internally by the compiler
 ;; The internals of define-record-type
-(define (record-type-factory . args)
-  (define rec-def (car args))
-
+(define (record-type-factory . rec-def)
+  
   (define type-id (next-record-type))
 
   (define name-clause (car rec-def))
@@ -45,24 +97,19 @@
   (define pred-clause (car (cdr (cdr rec-def))))
   (define fields-clause (cdr (cdr (cdr rec-def))))
 
-  ;; Setup the constructor .. this is subtlely +1 by choice
-  ;; (define field-count (length const-clause))
+  ;; setup the field count minus one for the name 
+  (define field-count (- (length const-clause) 1))
 
-  (display "Setting up define-record")
-  (newline)
-  (display "Name:")
-  (display name-clause)
-  (newline)
-  (display "Const:")
-  (display const-clause)
-  (newline)
-  (display "Predicate:")
-  (display pred-clause)
-  (newline)
-  (display "Fields:")
-  (display fields-clause)
-  (newline)
-  
+  ;; the record alist to bind
+  ;;  (cons name-clause type-id)
+  (define binding-list
+    (list
+      (cons pred-clause (record-predicate-factory type-id)) 
+      (cons (car const-clause) (record-constructor-factory type-id field-count))
+    ))
 
-  )
+  ;; Setup field accessors
+  (record-accessor-factory binding-list 1 (cdr const-clause) fields-clause))
+
+
 

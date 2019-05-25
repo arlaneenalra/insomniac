77c37a3c5679c88b5ddc2b69f05778238fea4044
arlaneenalra <emeraldd.chris@gmail.com>
2019-05-24 22:43:45 -0500

    Reference debug ranges in runtime.

--- src/libinsomniac_runtime/runtime_core.c
+++ src/libinsomniac_runtime/runtime_core.c
@@ -4,7 +4,12 @@ extern uint64_t scheme_code_size;
 extern uint8_t scheme_code[];
 
 extern char *debug_files[];
-extern int debug_files_count;
+extern uint64_t debug_files_count;
+
+extern debug_range_type debug_ranges[];
+extern uint64_t debug_ranges_count;
+
+
 
 int run_scheme(int argc, char **argv) {
     gc_type *gc = gc_create(sizeof(object_type));
@@ -14,6 +19,13 @@ int run_scheme(int argc, char **argv) {
     /* needed to setup locale aware printf . . .
        I need to do a great deal more research here */
     setlocale(LC_ALL, "");
+
+    /* Dump debug ranges. */ 
+    for (uint64_t i = 0; i < debug_ranges_count; i ++) {
+        printf("File: %s, Line: %" PRIi64 " Col: %" PRIi64 " Addr: %" PRIi64 "\n",
+            debug_ranges[i].file, debug_ranges[i].line, debug_ranges[i].column,
+            debug_ranges[i].start_addr);
+    }
 
     /* make this a root to the garbage collector */
     gc_register_root(gc, &vm);
--- src/libinsomniac_runtime/runtime_internal.h
+++ src/libinsomniac_runtime/runtime_internal.h
@@ -13,4 +13,14 @@
 
 #include <locale.h>
 
+/* Represents how debug data is written into the executable. */
+typedef struct debug_range {
+    char * file;
+
+    uint64_t line;
+    uint64_t column;
+    uint64_t start_addr;
+
+} debug_range_type;
+
 #endif

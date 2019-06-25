1278e8a05ec0ce611862f9b756f7436ca6ea677d
arlaneenalra <emeraldd.chris@gmail.com>
2019-06-25 17:59:01 -0500

    Add pushing things back into the stream.

--- test/test_core_stream.scm
+++ test/test_core_stream.scm
@@ -22,3 +22,26 @@
         (eof-object)
         #\4 #\3 #\2 #\1))
 
+(expect "Verify that we can put values back onto the stream"
+    (lambda ()
+        (with-input-port (open-input-string "1234")
+            (lambda ()
+                (define next-char (char-stream 2))
+
+                (list
+                    (next-char)
+                    (next-char)
+                    (next-char #\4)
+
+                    (next-char)
+                    (next-char)
+                    (next-char)
+                    (next-char)
+                    (next-char)))))
+    (list
+        (eof-object)
+        #\4
+        '()
+        (eof-object)
+        #\4 #\3 #\2 #\1))
+

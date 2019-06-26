5b4bf3f6f05dfc82dc1db1bba21e08ec7f5a8aaf
arlaneenalra <emeraldd.chris@gmail.com>
2019-06-25 23:38:04 -0500

    Fix count.

--- count.sh
+++ count.sh
@@ -4,7 +4,7 @@
 #echo
 
 echo "Source Statistics:"
-wc `find ./src ./lib -type f | grep -v 'test\/test_eyeball\|CMake\|build/\|\.*.txt\|\.*.swp\|.*log'` | sort -n
+wc `find ./src -type f | grep -v 'test\/test_eyeball\|CMake\|build/\|\.*.txt\|\.*.swp\|.*log'` | sort -n
 
 echo
 echo "Commit Count: " `git log | grep '^commit' | wc -l`
